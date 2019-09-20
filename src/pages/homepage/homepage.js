64c9826be3e155f5a2e211b6ed565a3ca69dd6cd
Sander Moen <smoensander@gmail.com>
2019-09-20 15:08:16 +0200

    started to refactor our scss into styled components

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/Directory';
 
-import './homePage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
 export const HomePage = () => {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 };
