9841495e329564db4ff03b3d64e12cf66860450e
Sander Moen <smoensander@gmail.com>
2019-09-25 15:32:37 +0200

    seperating our cra app into client folder, added express server to help handle backend stripe payment

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.post('/payment', (req, res, next) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      return res.status(500).send({ error: stripeErr });
+    }
+    res.send({ success: stripeRes });
+  });
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
